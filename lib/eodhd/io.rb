4a91b44911f388fdad3f5a70856655eb757d2fe1
Goran Mržljak <goran.mrzljak@gmail.com>
2025-12-31 22:35:07 +0100

    refactor io

--- lib/eodhd/io.rb
+++ lib/eodhd/io.rb
@@ -10,28 +10,38 @@ module Eodhd
     end
 
     def save_mcd_csv!(csv:)
+      save_csv!(relative_path: "data/MCD.US.csv", csv: csv)
+    end
+
+    def save_exchanges_list_json!(json:)
+      save_json!(relative_path: "exchanges-list.json", json: json, pretty: true)
+    end
+
+    private
+
+    def save_csv!(relative_path:, csv:)
       csv = Validate.required_string!("csv", csv)
+      relative_path = Validate.required_string!("relative_path", relative_path)
 
       write_text_file!(
-        relative_path: "data/MCD.US.csv",
+        relative_path: relative_path,
         content: csv,
         ensure_trailing_newline: false
       )
     end
 
-    def save_exchanges_list_json!(json:)
+    def save_json!(relative_path:, json:, pretty: true)
       json = Validate.required_string!("json", json)
+      relative_path = Validate.required_string!("relative_path", relative_path)
 
-      pretty = pretty_json(json)
+      content = pretty ? pretty_json(json) : json
 
       write_text_file!(
-        relative_path: "exchanges-list.json",
-        content: pretty,
+        relative_path: relative_path,
+        content: content,
         ensure_trailing_newline: true
       )
     end
-
-    private
 
     def pretty_json(json)
       JSON.pretty_generate(JSON.parse(json))
