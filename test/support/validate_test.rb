034f309a1c1c8d301a1f984be6bbf0d78aec2818
Goran Mržljak <goran.mrzljak@gmail.com>
2026-01-01 19:24:32 +0100

    refactor tests to use describe/it

--- test/support/validate_test.rb
+++ test/support/validate_test.rb
@@ -2,38 +2,42 @@
 
 require_relative "../test_helper"
 
-class ValidateTest < Minitest::Test
-  def test_required_string_strips
-    assert_equal "abc", Eodhd::Validate.required_string!("x", "  abc  ")
-  end
+describe Eodhd::Validate do
+  describe ".required_string!" do
+    it "strips whitespace" do
+      assert_equal "abc", Eodhd::Validate.required_string!("x", "  abc  ")
+    end
 
-  def test_required_string_raises_on_nil
-    assert_raises(ArgumentError) do
-      Eodhd::Validate.required_string!("x", nil)
+    it "raises on nil" do
+      assert_raises(ArgumentError) do
+        Eodhd::Validate.required_string!("x", nil)
+      end
+    end
+
+    it "raises on blank" do
+      assert_raises(ArgumentError) do
+        Eodhd::Validate.required_string!("x", "  d ")
+      end
     end
   end
 
-  def test_required_string_raises_on_blank
-    assert_raises(ArgumentError) do
-      Eodhd::Validate.required_string!("x", "   ")
+  describe ".http_url!" do
+    it "accepts http(s) and strips trailing slash" do
+      assert_equal "https://example.com", Eodhd::Validate.http_url!("base", "https://example.com/")
+      assert_equal "http://example.com/api", Eodhd::Validate.http_url!("base", "http://example.com/api")
     end
-  end
 
-  def test_http_url_accepts_http_and_strips_trailing_slash
-    assert_equal "https://example.com", Eodhd::Validate.http_url!("base", "https://example.com/")
-    assert_equal "http://example.com/api", Eodhd::Validate.http_url!("base", "http://example.com/api")
-  end
-
-  def test_http_url_rejects_non_http
-    err = assert_raises(ArgumentError) do
-      Eodhd::Validate.http_url!("base", "ftp://example.com")
+    it "rejects non-http urls" do
+      err = assert_raises(ArgumentError) do
+        Eodhd::Validate.http_url!("base", "ftp://example.com")
+      end
+      assert_match(/must start with http:\/\/ or https:\/\//, err.message)
     end
-    assert_match(/must start with http:\/\/ or https:\/\//, err.message)
-  end
 
-  def test_http_url_rejects_blank
-    assert_raises(ArgumentError) do
-      Eodhd::Validate.http_url!("base", "")
+    it "rejects blank" do
+      assert_raises(ArgumentError) do
+        Eodhd::Validate.http_url!("base", "")
+      end
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,5 @@
 $LOAD_PATH.unshift(File.expand_path("../lib", __dir__))
 
 require "minitest/autorun"
+require "minitest/spec"
 require "eodhd"
