961d6dbf232b488578356b51f38c2fe1d3a8064c
Goran Mržljak <goran.mrzljak@gmail.com>
2026-01-01 16:53:45 +0100

    move Validate to support dir

--- lib/eodhd.rb
+++ lib/eodhd.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "eodhd/validate"
+require_relative "support/logger"
+require_relative "support/validate"
 require_relative "eodhd/config"
 require_relative "eodhd/eodhd_api"
 require_relative "eodhd/io"
-require_relative "support/logger"
 require_relative "eodhd/fetch"
 
 module Eodhd
