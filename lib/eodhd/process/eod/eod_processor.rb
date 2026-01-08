e1bfd6a6b71fff457a9aa4e8f9bd9e88c7fafcba
Goran Mržljak <goran.mrzljak@gmail.com>
2026-01-08 21:12:00 +0100

    update eod processing

--- lib/eodhd/process/eod/eod_processor.rb
+++ lib/eodhd/process/eod/eod_processor.rb
@@ -20,19 +20,30 @@ module Eodhd
     def process_csv(raw_csv, splits)
       parsed_rows = EodCsvParser.parse(raw_csv)
 
+      results = parsed_rows.map do |row|
+        factor = PriceAdjuster.cumulative_split_factor_for_date(row[:date], splits)
+
+        {
+          date: row[:date].iso8601,
+          open: PriceAdjuster.adjust_price(row[:open].to_s("F"), factor),
+          high: PriceAdjuster.adjust_price(row[:high].to_s("F"), factor),
+          low: PriceAdjuster.adjust_price(row[:low].to_s("F"), factor),
+          close: PriceAdjuster.adjust_price(row[:close].to_s("F"), factor),
+          volume: PriceAdjuster.adjust_volume(row[:volume].to_s, factor)
+        }
+      end
+
       CSV.generate do |out_csv|
         out_csv << OUTPUT_HEADERS
 
-        parsed_rows.each do |row|
-          factor = PriceAdjuster.cumulative_split_factor_for_date(row[:date], splits)
-
+        results.each do |row|
           out_csv << [
-            row[:date].iso8601,
-            PriceAdjuster.adjust_price(row[:open].to_s("F"), factor),
-            PriceAdjuster.adjust_price(row[:high].to_s("F"), factor),
-            PriceAdjuster.adjust_price(row[:low].to_s("F"), factor),
-            PriceAdjuster.adjust_price(row[:close].to_s("F"), factor),
-            PriceAdjuster.adjust_volume(row[:volume].to_s, factor)
+            row[:date],
+            row[:open],
+            row[:high],
+            row[:low],
+            row[:close],
+            row[:volume]
           ]
         end
       end
