ccb64e5f3f89c34381f926708354fef501e76bf1
Goran Mržljak <goran.mrzljak@gmail.com>
2026-01-09 16:08:41 +0100

    remove some unused code

--- lib/eodhd/process/shared/price_adjuster.rb
+++ lib/eodhd/process/shared/price_adjuster.rb
@@ -17,24 +17,6 @@ module Eodhd
       def adjust_volume(value, factor)
         (value * factor).to_i
       end
-
-      private
-
-      def upper_bound_split_date(splits, date)
-        lo = 0
-        hi = splits.length
-
-        while lo < hi
-          mid = (lo + hi) / 2
-          if splits[mid].date <= date
-            lo = mid + 1
-          else
-            hi = mid
-          end
-        end
-
-        lo
-      end
     end
   end
 end
