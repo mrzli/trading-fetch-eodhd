95c7e7f4d3da668516be2bb00111a6da5007e3c9
Goran Mržljak <goran.mrzljak@gmail.com>
2026-01-09 16:17:53 +0100

    small performance improvement

--- lib/eodhd/process/eod/eod_adjust.rb
+++ lib/eodhd/process/eod/eod_adjust.rb
@@ -20,19 +20,16 @@ module Eodhd
             row
           else
             factor = segments[idx][:factor]
-            if factor == 1 || factor == Rational(1, 1)
-              row
-            else
-              {
-                timestamp: ts,
-                date: row[:date],
-                open: adjust_price(row[:open], factor),
-                high: adjust_price(row[:high], factor),
-                low: adjust_price(row[:low], factor),
-                close: adjust_price(row[:close], factor),
-                volume: adjust_volume(row[:volume], factor)
-              }
-            end
+
+            {
+              timestamp: ts,
+              date: row[:date],
+              open: adjust_price(row[:open], factor),
+              high: adjust_price(row[:high], factor),
+              low: adjust_price(row[:low], factor),
+              close: adjust_price(row[:close], factor),
+              volume: adjust_volume(row[:volume], factor)
+            }
           end
         end
       end
