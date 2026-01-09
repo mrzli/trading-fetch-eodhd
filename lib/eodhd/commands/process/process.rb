7367f8d466fa3a44dedda05954cb5d3282fafba5
Goran Mržljak <goran.mrzljak@gmail.com>
2026-01-09 15:57:55 +0100

    refactor arg processing for process command

---
+++ lib/eodhd/commands/process/process.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "json"
+
+require_relative "../../../util"
+require_relative "../../process/process_strategy"
+require_relative "../../shared/config"
+require_relative "../../shared/io"
+
+module Eodhd
+  module Process
+    module_function
+
+    def run(mode: "eod", exchange_filters: [], symbol_filters: [])
+      log = Logger.new
+
+      begin
+        cfg = Config.eodhd
+      rescue Config::Error => e
+        abort e.message
+      end
+
+      io = Io.new(output_dir: cfg.output_dir)
+
+      strategy = ProcessStrategy.new(
+        log: log,
+        cfg: cfg,
+        io: io
+      )
+
+      mode = mode.to_s.strip.downcase
+      case mode
+      when "eod"
+        strategy.process_eod(exchange_filters: exchange_filters, symbol_filters: symbol_filters)
+      when "intraday"
+        strategy.process_intraday(exchange_filters: exchange_filters, symbol_filters: symbol_filters)
+      else
+        raise ArgumentError, "Unknown mode: #{mode.inspect}. Expected 'eod' or 'intraday'."
+      end
+    end
+  end
+end
